4fed766b486988ab314b801eaa5b10f5f2b5d536
Lê Gia Thịnh <vanquile789@gmail.com>
2026-07-22 13:24:23 +0700

    fix app + add templates

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+tasks = []
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "<h1>Employee Task Scheduler</h1>"
+    if request.method == "POST":
+        task = request.form.get("task")
+        if task:
+            tasks.append(task)
+        return redirect("/")
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/delete/<int:index>")
+def delete(index):
+    if 0 <= index < len(tasks):
+        tasks.pop(index)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
